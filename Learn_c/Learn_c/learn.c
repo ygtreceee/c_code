54e05eac488059f5d7f17d26c9c6f2cbfa610f75
ygtrece <643788048@qq.com>
2022-09-13 14:41:18 +0800

    c

--- Learn_c/Learn_c/learn.c
+++ Learn_c/Learn_c/learn.c
@@ -256,3 +256,58 @@ int main()
 
 
 
+//判断一个数的二进制中有几个1
+//方法一
+int Numberof1(int a)
+{
+	int count = 0;
+	int i = 0;
+	for (i = 0; i < 32; i++)
+	{
+		if ((a >> i) & 1 == 1)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	int num= Numberof1(a);
+	printf("%d", num);
+	return 0;
+}
+//方法二
+int main()
+{
+	int count = 0;
+	int n = 10;
+	while (n)
+	{
+		n = n & (n - 1);//利用n&（n-1）会使得其二进制数最后一位变为0，直到全部位都变为0即可
+		count++;
+	}
+	printf("%d", count);
+	return 0;
+}
+
+
+//判断一个数是不是2的n次方
+//n&(n-1)==0即为所求 
+
+
+//判断两个整数二进制格式有多少个位不同
+int main()
+{
+	int m = 0;
+	int n = 0;
+	int i = 0;
+	int count = 0;
+	for (i = 0; i < 32; i++
+	{
+		(m>>i)&1
+	}
+	return 0;
+}
