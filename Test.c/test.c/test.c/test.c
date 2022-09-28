4fdade7dd4f403da55cf5da96f08e14658abe46a
ygtreceee <643788048@qq.com>
2022-09-28 23:02:52 +0800

    c

--- Test.c/test.c/test.c/test.c
+++ Test.c/test.c/test.c/test.c
@@ -1,34 +1,98 @@
+//
 //#include <stdio.h>
+//int g(int n, int m)
+//{
+//	int sum = 0;
+//	while (n)
+//	{
+//		sum += n % m;
+//		n = n / m;
+//	}
+//	return sum;
+//}
 //int main()
 //{
-//	char arr[100],max;
-//	while (gets(arr)!= EOF)
+//	int a,b,c,n;
+//	while (scanf_s("%d", &n) != EOF && n!=0)
 //	{
-//		max = arr[0];
-//		for (int i = 0;arr[i] !='\0'; i++)
+//		a = g(n, 10);
+//		b = g(n, 12);
+//		c = g(n, 16);
+//		if (a==b && a==c && b==c)
 //		{
-//			if (arr[i] > max)
-//			{
-//				max = arr[i];
-//			}
+//			printf("%ld is a Sky Number.\n",n);
 //		}
-//		for (int i = 0; arr[i] != '\0'; i++)
+//		else
 //		{
-//			printf("%c", arr[i]);
-//			if (arr[i]==max)printf("(max)");
+//			printf("%ld is not a Sky Number.\n",n);
 //		}
-//		printf("\n");
 //	}
 //	return 0;
 //}
 
 
 
+//#include <stdio.h>
+//int main()
+//{
+//	char arr[30] = { 0 };
+//	char a, b;
+//	while (scanf("%s", arr) != EOF)
+//	{
+//		getchar();
+//		scanf("%c", &a);
+//		getchar();
+//		scanf("%c", &b);
+//		int i = 0;
+//		while(arr[i]!='\0')
+//		{
+//			if (arr[i] == a)
+//			{
+//				arr[i] = b;
+//			}
+//			i++;
+//		}
+//		printf("%s\n", arr);
+//	}
+//}
+
+
+
+//ถþฮฌสýื้
+
 #include <stdio.h>
 int main()
 {
-	char arr[100];
-	gets(arr);
-	printf("%s", arr);
-	return 0;
+	int n = 0,count=0;
+	char a[100][200];
+	int b[100];
+	scanf("%d", &n);
+	getchar();
+	for (int i = 0; i < n; i++)
+	{
+		gets(a[i]);
+		for (int j = 0; a[i][j] != '\0'; j++)
+		{
+			if (a[i][j] == 1)
+			{
+				count++;
+			}
+		}
+		b[i] = count;
+		count = 0;
+	}
+		int x = 0;
+		int tmp=0;
+		for (int i=0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (b[j] > tmp && j != x)
+				{
+					x = j;
+					tmp = b[j];
+				}
+			}
+			printf("%s\n", a[x]);
+		}
 }
